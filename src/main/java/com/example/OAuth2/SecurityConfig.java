9c07b0234bb541d54887af162c0d50fd14648292
V3NOM1977 <rishabhrawat1977@gmail.com>
2023-12-22 22:26:21 +0530

    OPaque and non opaque tokens

--- src/main/java/com/example/OAuth2/SecurityConfig.java
+++ src/main/java/com/example/OAuth2/SecurityConfig.java
@@ -1,24 +1,37 @@
 package com.example.OAuth2;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    @Value("${jwksUri}")
-    private String jwksUri;
+    // NON OPAQUE TOKENS
+    // @Bean
+    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
+    // Exception {
+    // http.oauth2ResourceServer(
+    // r -> r.jwt(jwt -> jwt
+    // .jwkSetUri("http://localhost:8080/oauth2/jwks")
+    // .jwtAuthenticationConverter(new CustomJwtAuthenticationTokenConverter()))
 
+    // );
+
+    // http.authorizeHttpRequests(auth -> {
+    // auth.anyRequest().authenticated();
+    // });
+    // return http.build();
+    // }
+
+    // OPAQUE TOKENS
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.oauth2ResourceServer(
-            r -> r.jwt().jwkSetUri(jwksUri)
-        // .jwtAuthenticationConverter(new CustomJwtAuthenticationTokenConverter())
-        );
+        http.oauth2ResourceServer(oauth2 -> oauth2
+                .opaqueToken(opaqueToken -> opaqueToken
+                        .introspectionUri("http://localhost:8080/oauth2/introspect")
+                        .introspectionClientCredentials("client", "secret")));
 
         http.authorizeHttpRequests(auth -> {
             auth.anyRequest().authenticated();
